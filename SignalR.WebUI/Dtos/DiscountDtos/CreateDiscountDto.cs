8b570259d3e1f16e4e14658cfda36e24c2500c54
xeneskus <xeneskus@gmail.com>
2024-03-09 18:28:52 +0300

    Added DiscountController for MVC (Api Consume)

---
+++ SignalR.WebUI/Dtos/DiscountDtos/CreateDiscountDto.cs
@@ -0,0 +1,11 @@
+﻿namespace SignalR.WebUI.Dtos.DiscountDtos
+{
+    public class CreateDiscountDto
+    {
+        public string Title { get; set; }
+        public string Amount { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public bool Status { get; set; }
+    }
+}
---
+++ SignalR.WebUI/Dtos/DiscountDtos/GetDiscountDto.cs
@@ -0,0 +1,11 @@
+﻿namespace SignalR.WebUI.Dtos.DiscountDtos
+{
+    public class GetDiscountDto
+    {
+        public int DiscountID { get; set; }
+        public string Title { get; set; }
+        public string Amount { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
