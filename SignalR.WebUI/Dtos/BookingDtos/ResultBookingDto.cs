4884e2e61638de8a8c8b4a6683ab6a158b81cfa4
xeneskus <xeneskus@gmail.com>
2024-03-09 18:10:39 +0300

    Added BookingController for MVC (Api Consume)

---
+++ SignalR.WebUI/Dtos/BookingDtos/ResultBookingDto.cs
@@ -0,0 +1,13 @@
+﻿namespace SignalR.WebUI.Dtos.BookingDtos
+{
+    public class ResultBookingDto
+    {
+        public int BookingID { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Mail { get; set; }
+        public int PersonCount { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+    }
+}
