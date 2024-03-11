5eb418df1b9956ce84d26e59a747f5cd1211e763
xeneskus <xeneskus@gmail.com>
2024-03-11 21:37:57 +0300

    Added fetching the number of categories with signalr

--- SignalR.WebUI/Controllers/SignalRDefaultController.cs
+++ SignalR.WebUI/Controllers/SignalRDefaultController.cs
@@ -8,5 +8,9 @@ namespace SignalR.WebUI.Controllers
         {
             return View();
         }
+        public IActionResult Index2()
+        {
+            return View();
+        }
     }
 }
