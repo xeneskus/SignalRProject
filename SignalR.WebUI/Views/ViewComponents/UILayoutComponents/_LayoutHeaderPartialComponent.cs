0e6c88ba436b55f893bb565ebe4f5a9522100d2f
xeneskus <xeneskus@gmail.com>
2024-03-27 15:37:50 +0300

    Added UILayoutHead ViewComponent

---
+++ SignalR.WebUI/Views/ViewComponents/UILayoutComponents/_LayoutHeaderPartialComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalR.WebUI.Views.ViewComponents.UILayoutComponents
+{
+    public class _LayoutHeaderPartialComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
