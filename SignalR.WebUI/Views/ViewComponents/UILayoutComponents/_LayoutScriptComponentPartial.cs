5a57937e6ff711f9a2417a6b71f459bf3a3522ee
xeneskus <xeneskus@gmail.com>
2024-03-27 15:58:05 +0300

    Added LayoutScriptComponentPartial

---
+++ SignalR.WebUI/Views/ViewComponents/UILayoutComponents/_LayoutScriptComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SignalR.WebUI.Views.ViewComponents.UILayoutComponents
+{
+	public class _LayoutScriptComponentPartial : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
