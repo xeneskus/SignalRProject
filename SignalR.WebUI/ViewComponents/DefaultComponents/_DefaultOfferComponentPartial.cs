b7e98f9df89768d72c0d9a1b69d9114a64b06264
xeneskus <xeneskus@gmail.com>
2024-03-28 16:10:36 +0300

    Added Default Offer Component Partial

---
+++ SignalR.WebUI/ViewComponents/DefaultComponents/_DefaultOfferComponentPartial.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalR.WebUI.Dtos.DiscountDtos;
+
+namespace SignalR.WebUI.ViewComponents.DefaultComponents
+{
+    public class _DefaultOfferComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _DefaultOfferComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7136/api/Discount/GetListByStatusTrue");
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultDiscountDto>>(jsonData);
+            return View(values);
+        }
+    }
+}
