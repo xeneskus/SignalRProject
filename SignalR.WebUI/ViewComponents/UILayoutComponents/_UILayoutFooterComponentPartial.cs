7a3c55f198f2d4794501c93ff9ccc77ef498d63c
xeneskus <xeneskus@gmail.com>
2024-03-29 15:58:30 +0300

    Completed UILayoutFooterComponentPartial

--- SignalR.WebUI/ViewComponents/UILayoutComponents/_UILayoutFooterComponentPartial.cs
+++ SignalR.WebUI/ViewComponents/UILayoutComponents/_UILayoutFooterComponentPartial.cs
@@ -1,12 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalR.WebUI.Dtos.ContactDtos;
 
 namespace SignalR.WebUI.ViewComponents.UILayoutComponents
 {
 	public class _UILayoutFooterComponentPartial : ViewComponent
 	{
-		public IViewComponentResult Invoke()
-		{
-			return View();
-		}
-	}
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _UILayoutFooterComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7136/api/Contact");
+            var jsonData = await responseMessage.Content.ReadAsStringAsync();
+            var values = JsonConvert.DeserializeObject<List<ResultContactDto>>(jsonData);
+            return View(values);
+        }
+    }
 }
