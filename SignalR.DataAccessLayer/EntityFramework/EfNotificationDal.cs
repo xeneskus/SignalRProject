fb72aad4de99fb66fda5e097f3c599ef57b30f9c
xeneskus <xeneskus@gmail.com>
2024-04-12 23:40:43 +0300

    Completed EfNotificationDal

--- SignalR.DataAccessLayer/EntityFramework/EfNotificationDal.cs
+++ SignalR.DataAccessLayer/EntityFramework/EfNotificationDal.cs
@@ -13,22 +13,30 @@ namespace SignalR.DataAccessLayer.EntityFramework
 
         public List<Notification> GetAllNotificationByFalse()
         {
-            throw new NotImplementedException();
+            using var context = new SignalRContext();
+            return context.Notifications.Where(x => x.Status == false).ToList();
         }
 
         public int NotificationCountByStatusFalse()
         {
-            throw new NotImplementedException();
+            using var context = new SignalRContext();
+            return context.Notifications.Where(x => x.Status == false).Count();
         }
 
         public void NotificationStatusChangeToFalse(int id)
         {
-            throw new NotImplementedException();
+            using var context = new SignalRContext();
+            var value = context.Notifications.Find(id);
+            value.Status = false;
+            context.SaveChanges();
         }
 
         public void NotificationStatusChangeToTrue(int id)
         {
-            throw new NotImplementedException();
+            using var context = new SignalRContext();
+            var value = context.Notifications.Find(id);
+            value.Status = true;
+            context.SaveChanges();
         }
     }
 }
